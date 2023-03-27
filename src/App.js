9ca090915727c32e19a91da8107740149976df88
ibrahim-sert <evo3506@gmail.com>
2023-03-28 02:03:53 +0300

    changes edited

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import Main from "./pages/Main";
 import NewProduct from "./pages/NewProduct";
 import ProductList from "./pages/ProductList";
@@ -8,22 +7,16 @@ import Navbar from "./components/Navbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-
   return (
     <div className="bg-light app">
-      {/* <Navbar/>
-      <Main/>
-      <About/>
-      <NewProduct/>
-      <ProductList/> */}
       <BrowserRouter>
         <Navbar />
         <Routes>
           <Route path="/" element={<Main />} />
-          <Route path="/new-product" element={<NewProduct/>} />
-          <Route path="/product-list" element={<ProductList/>} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/update-product" element={<UpdateProduct/>}/>
+          <Route path="/new-product" element={<NewProduct />} />
+          <Route path="/product-list" element={<ProductList />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/update-product" element={<UpdateProduct />} />
         </Routes>
       </BrowserRouter>
     </div>
